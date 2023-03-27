70bfd05b55fe7a0e71e529988a5d805a933e1904
Lasitha Jananjaya <lasithathilakarathne99@gmail.com>
2023-03-27 17:49:31 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,91 @@
-FUNCTION mergeSort(array, first, last) BEGIN
-IF first < last THEN
-mid <- INTEGER((first + last) / 2)
-mergeSort(array, first, mid)
-mergeSort(array, mid + 1, last)
-merge(array, first, mid, last)
-END FUNCTION
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string lefttrim(const string &);
+string righttrim(const string &);
+vector<string> split(const string &);
+
+
+/*
+ * Complete the 'super_digit' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING n
+ *  2. INTEGER k
+ */
+
+int super_digit(string n, int k) {
+    if ( n.length() == 1) {
+        return stoi(n);
+    }
+    long total = 0;
+    for (char& element : n){
+        total += element - 48;
+    }
+    total *=k;
+    return super_digit(to_string(total),1);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(righttrim
+(first_multiple_input_temp));
+
+    string n = first_multiple_input[0];
+
+    int k = stoi(first_multiple_input[1]);
+
+    int result = super_digit(n, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string lefttrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string righttrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
